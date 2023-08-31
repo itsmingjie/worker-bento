e8e786da49e08aaf3f52bc70e847ffa876ec1230
Mingjie Jiang <itsmingjie@gmail.com>
2023-08-30 21:32:23 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+export interface Env {
+	BASE_URL: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const requestUrl = new URL(request.url);
+		let someHost = 'https://bento.me';
+		const path = requestUrl.pathname;
+
+		if (path.includes('v1')) {
+			someHost = 'https://api.bento.me';
+		}
+
+		let url = someHost + requestUrl.pathname;
+		if (url === 'https://bento.me/') {
+			url = 'https://bento.me/itsmingjie';
+		}
+
+		const init = {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+				'content-type': 'text/plain;charset=UTF-8',
+			},
+		};
+
+		async function gatherResponse(response: Response, contentType: string) {
+			if (contentType.includes('application/json')) {
+				return JSON.stringify(await response.json());
+			} else if (contentType.includes('application/text')) {
+				return response.text();
+			} else if (contentType.includes('text/html')) {
+				return response.text();
+			} else {
+				return response.text();
+			}
+		}
+
+		const response = await fetch(url, init);
+		const contentType = response.headers.get('content-type')!;
+		let results = await gatherResponse(response, contentType);
+		results = results.replaceAll('https://api.bento.me', env.BASE_URL);
+
+		init.headers['content-type'] = contentType;
+		return new Response(results, init);
+	},
+};
